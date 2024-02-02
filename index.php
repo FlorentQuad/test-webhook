a448879e6eb2c6ddfe8b32cd0fa48e181dac9c14
FlorentQuad <152967116+FlorentQuad@users.noreply.github.com>
2024-02-02 18:25:00 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,9 +1,9 @@
 <html> 
 <head> 
-  <title>S2i Test</title>
+  <title>Demo Pipeline</title>
   <meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1"/>
 </head>
   <body> 
-  <h1>HACK</h1>
+    <h1>Henk</h1>
   </body> 
 </html>
