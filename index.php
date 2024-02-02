b4f53f6e71a70bda8a5eeeffb4773df3e06ba939
FlorentQuad <152967116+FlorentQuad@users.noreply.github.com>
2024-02-02 18:26:22 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,9 +1,9 @@
 <html> 
-<head> 
-  <title>Demo Pipeline</title>
-  <meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1"/>
-</head>
+  <head> 
+    <title>Demo Pipeline</title>
+    <meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1"/>
+  </head>
   <body> 
-    <h1>Henk</h1>
+    <h1>Henk 123</h1>
   </body> 
 </html>
