4e70f08e8fdfc112f9d30c861b9142f32b1f0785
FlorentQuad <152967116+FlorentQuad@users.noreply.github.com>
2024-02-03 11:19:59 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,9 +1,45 @@
-<html> 
-  <head> 
+<html>
+
+<head>
     <title>Demo Pipeline</title>
-    <meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1"/>
-  </head>
-  <body> 
-    <h1>WARZONE</h1>
-  </body> 
+    <meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1" />
+    <style>
+        body {
+            display: flex;
+            align-items: center;
+            justify-content: center;
+        }
+
+        button {
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            color: rgb(239, 239, 239);
+            background-color: rgb(2, 2, 2);
+            border: none;
+            min-width: min(16rem, 80%);
+            max-width: 100%;
+            aspect-ratio: 1/1;
+
+            font-size: 3rem;
+            font-family: monospace;
+
+            padding: 2rem;
+            border-radius: 100%;
+            transition: 0.3s all;
+
+            cursor: pointer;
+        }
+
+        button:hover {
+            transform: scale(1.2);
+            border-radius: 1rem;          
+        }
+    </style>
+</head>
+
+<body>
+    <button>Demo</button>
+</body>
+
 </html>
